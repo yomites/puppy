3e6f6ebfcceb878131673bd7172a7a959599b438
Oladele Oriyomi <marshall_400@yahoo.com>
2026-02-24 22:52:12 +0200

    modifications to puppy.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yomites/puppy
 
 go 1.25.0
+
+require github.com/yomites/dog v0.0.0-20260224204628-ff809c379822 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/yomites/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(Bark())
 }
