5d396c7dc8349ac0bf4524eb007cd1415595ad76
Oladele Oriyomi <marshall_400@yahoo.com>
2026-02-24 23:58:45 +0200

    version 1.0.0

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/yomites/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrowUp(Barks())
+}
+
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
 }
